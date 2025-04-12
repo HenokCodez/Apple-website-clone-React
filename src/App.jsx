8df96ca13e85cfff22fe1fceef34ea868687c8c1
Henok <zakbinayew@gmail.com>
2025-04-12 14:47:30 +0300

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./commonResource/css/bootstrap.css";
+import "./commonResource/css/styles.css";
+import Header from "./components/Header";
+import Alert from "./components/Alert";
+import S1 from "./components/S1";
+import S2 from "./components/S2";
+import S3 from "./components/S3";
+import S4 from "./components/S4";
+import S5 from "./components/S5";
+import S6 from "./components/S6";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Alert />
+      <S1 />
+      <S2 />
+      <S3 />
+      <S4 />
+      <S5 />
+      <S6 />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
